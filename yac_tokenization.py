229c32455e9c7e9fdde88dc51c539de3dbbbf5f8
PITOLIN Kevin <kevin.pitolin@me.com>
2016-10-04 01:11:01 +0200

    Corrected tokenization
    -doc_word_list is set to an empty list rather than destroyed (before, it crashed after the end of the first document)
    -it was only taking 1 digit ints for the docid -> corrected regex
    -added some unit tests to catch that bug
    -added lines to do performance testing (commented out at the end).
    
    On my machine (MacbookPro 13inch Core2Duo 4Go of memory), the average processing time for file latimes/la010189 (192 documents) 8.01 sec with XML tags and 6.53 without

--- yac_tokenization.py
+++ yac_tokenization.py
@@ -1,4 +1,4 @@
-import nltk, unittest, re, os
+import nltk, unittest, re, os, time
 
 class TextFile:
 	"""
@@ -22,14 +22,26 @@ class TextFile:
 		with open(self.filepath,'w') as text_file:
 			return text_file.write(stringContent)
 
-	def tokenizeTextFileByDocNltk(self):
+	def tokenizeTextFileByDocNltk(self,without_tags=False):
 		""" Extracts the words out of a text file
 		"""
 
 		with open(self.filepath,'r') as raw_text:
-			return TextFile.tokenizeStringByDocNltk(raw_text)
+			return TextFile.tokenizeStringByDocNltk(raw_text,without_tags)
 
 		return {}
+
+	@staticmethod
+	def filterTags(string):
+		"""Returns a string without tags
+		"""
+	
+		doc = nltk.regexp_tokenize(string, r"</?[\w]+>", gaps=True)
+		doc = list(map(lambda item: re.sub(r"(\n|\t)", "", item), doc))
+		doc = list(filter(lambda item: item != "", doc))
+		doc = "".join(doc)
+
+		return doc
 
 	# execute nltk.download() to download corpora before executing that function
 	@staticmethod
@@ -41,26 +53,21 @@ class TextFile:
 		dictionnary_doc_words = {}
 		doc_word_list = []
 		doc_id = ''
-		lines = ""
+		lines = []
 
 		#textfile
 		if hasattr(text, 'readlines'):
 			lines = text
-		#multi-line string 
+			#multi-line string 
 		elif isinstance(text,str):
 			lines = text.splitlines(False)
 
 		for line in lines:
+
 			#extract the tokens out of the raw text
-			if without_tags:
-				tokens = TextFile.filterTags(line)
-				tokens = nltk.word_tokenize(tokens)
-			else:
-				tokens = nltk.word_tokenize(line)
-
-
+			tokens = nltk.word_tokenize(TextFile.filterTags(line)) if without_tags else nltk.word_tokenize(line)
 			doc_word_list += tokens
-			pattern_doc_id = r"<DOCID>\s(\d)+\s</DOCID>"
+			pattern_doc_id = r"<DOCID>\s(\d+)\s</DOCID>"
 			pattern_doc_end = r"</DOC>"
 
 			match = re.search(pattern_doc_id, line)
@@ -71,21 +78,10 @@ class TextFile:
 			elif re.search(pattern_doc_end, line) and doc_word_list != [] and doc_id != '':
 				#if we reached the end of the document, insert tokens in hashmap and flush variables
 				dictionnary_doc_words[doc_id] = doc_word_list
-				del doc_word_list
+				doc_word_list = []
 				del doc_id
 
 		return dictionnary_doc_words
-
-	@staticmethod
-	def filterTags(string):
-		"""Returns a string without tags"""
-	
-		doc = nltk.regexp_tokenize(string, r"</?[\w]+>", gaps=True)
-		doc = list(map(lambda item: re.sub(r"(\n|\t)", "", item), doc))
-		doc = list(filter(lambda item: item != "", doc))
-		doc = "".join(doc)
-
-		return doc
 
 	@classmethod
 	def new_file(cls,filepath):
@@ -100,13 +96,13 @@ class TextFile:
 # Just execute the script to run the tests for now (see the two last lines)
 class TokenizationTestCase(unittest.TestCase):
 	def setUp(self):
-		self.textFileNF = TextFile("")
 		self.emptyFile = TextFile.new_file("emptyTest.txt")
 
 
 	def test_tokenize_file_notFound(self):
+		textFileNF = TextFile("")
 		with self.assertRaises(IOError):
-			self.textFileNF.tokenizeTextFileByDocNltk()
+			textFileNF.tokenizeTextFileByDocNltk()
 
 	def test_filterTags_empty(self):
 		self.assertEqual(TextFile.filterTags(""), "")
@@ -136,12 +132,36 @@ class TokenizationTestCase(unittest.TestCase):
 		</DOC>"""
 		self.assertEqual(TextFile.tokenizeStringByDocNltk(stringNormalOneDoc),{1: ['<', 'DOC', '>', '<', 'DOCID', '>', '1', '<', '/DOCID', '>', 'The', 'onset', 'of', 'the', 'new', 'Gorbachev', '<', '/DOC', '>']})
 
+	def test_tokenize_two_documents_string_normal_no_special_characters(self):
+		stringNormalOneDoc = """<DOC>
+		<DOCID> 1 </DOCID>
+		The onset of the new Gorbachev
+		</DOC>
+		<DOC>
+		<DOCID> 2 </DOCID>
+		The onset of the new Gorbachev
+		</DOC>"""
+		self.assertEqual(TextFile.tokenizeStringByDocNltk(stringNormalOneDoc),{1: ['<', 'DOC', '>', '<', 'DOCID', '>', '1', '<', '/DOCID', '>', 'The', 'onset', 'of', 'the', 'new', 'Gorbachev', '<', '/DOC', '>'], 2: ['<', 'DOC', '>', '<', 'DOCID', '>', '2', '<', '/DOCID', '>', 'The', 'onset', 'of', 'the', 'new', 'Gorbachev', '<', '/DOC', '>']})
+
+
+
 	def test_tokenize_one_document_string_normal_no_special_characters_without_tags(self):
 		stringNormalOneDoc = """<DOC>
 		<DOCID> 1 </DOCID>
 		The onset of the new Gorbachev
 		</DOC>"""
 		self.assertEqual(TextFile.tokenizeStringByDocNltk(stringNormalOneDoc,True),{1: ['1','The', 'onset', 'of', 'the', 'new', 'Gorbachev']})
+
+	def test_tokenize_two_documents_string_normal_no_special_characters_without_tags(self):
+		stringNormalOneDoc = """<DOC>
+		<DOCID> 1 </DOCID>
+		The onset of the new Gorbachev
+		</DOC>
+		<DOC>
+		<DOCID> 2 </DOCID>
+		The onset of the new Gorbachev
+		</DOC>"""
+		self.assertEqual(TextFile.tokenizeStringByDocNltk(stringNormalOneDoc,True),{1: ['1','The', 'onset', 'of', 'the', 'new', 'Gorbachev'], 2: ['2','The', 'onset', 'of', 'the', 'new', 'Gorbachev']})
 
 
 
@@ -150,10 +170,10 @@ class TokenizationTestCase(unittest.TestCase):
 		<DOCID> 1 </DOCID>
 		The onset of the new Gorbachev
 		</DOC>"""
-		testFile = TextFile.new_file("stringNormalOneDoc")
-		testFile.write(stringNormalOneDoc)
-		self.assertEqual(testFile.tokenizeTextFileByDocNltk(),{1: ['<', 'DOC', '>', '<', 'DOCID', '>', '1', '<', '/DOCID', '>', 'The', 'onset', 'of', 'the', 'new', 'Gorbachev', '<', '/DOC', '>']})
-		testFile.deleteFile()
+		tFile = TextFile.new_file("stringNormalOneDoc")
+		tFile.write(stringNormalOneDoc)
+		self.assertEqual(tFile.tokenizeTextFileByDocNltk(),{1: ['<', 'DOC', '>', '<', 'DOCID', '>', '1', '<', '/DOCID', '>', 'The', 'onset', 'of', 'the', 'new', 'Gorbachev', '<', '/DOC', '>']})
+		tFile.deleteFile()
 		
 	#def test_tokenize_one_document_without_doc_tag_start(self):
 	#	self.assertEqual(TextFile.tokenizeStringByDocNltk('emptyFile'),{"":""})
@@ -176,4 +196,11 @@ class TokenizationTestCase(unittest.TestCase):
 
 if __name__=='__main__':
 	unittest.main()
+
+#start = time.clock()
+#t = TextFile("pathToFile") # '../../../../Downloads/latimes/la010189'
+#hashmap = t.tokenizeTextFileByDocNltk(True)
+#print(hashmap)
+#end = time.clock()
+#print(end - start)
 
