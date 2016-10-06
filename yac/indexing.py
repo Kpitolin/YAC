3991fdaa6445050664044cb43af44d3d30e83732
arthurpichotutrera <arthur.pichot-utrera@orange.fr>
2016-10-06 11:45:27 +0200

    WIP on indexing

--- yac/indexing.py
+++ yac/indexing.py
@@ -1,38 +1,39 @@
 """This file allows you to create an Inverted Index."""
 
+import glob
+import re
 import tokenization
 
 pattern_doc_id = r"<DOCID>\s(\d+)\s</DOCID>"
 pattern_doc_end = r"</DOC>"
 
-#creation of the Inverted Index
-ii = {}
+def createIndex(filePathFormat):
+	#creation of the Inverted Index
+	ii = {}
 
-#filling of the Inverted Index
-for filename in glob.glob("../../latimes/la010189"):
-  lines = open(filename, 'r')
-  
-	doc_id = ''
-	doc = ''
-
-	for line in lines:
-		match = re.search(pattern_doc_id, line)
-		if match:
-			#extract the docid from the line : the first group in the regex (what's between parenthesis)
-			doc_id = int(match.group(1))
-
-		elif re.search(pattern_doc_end, line) and doc_word_list != [] and doc_id != '':
-			#if we reached the end of the document, insert tokens in hashmap and flush variables
-			words = tokenize(doc)
-			
-		  score = 1.0/len(words)
-			for word in words:
-				if not word in ii:
-					ii[word] = {}
-				if not filename in ii[word]:
-					ii[word][filename] = score
-				else:
-					ii[word][filename] += score
-			doc = ''
-			del doc_id
+	#filling of the Inverted Index
+	for filename in glob.glob(filePathFormat):
+		lines = open(filename, 'r')
+		doc_id = ''
+		doc = ''
+		for line in lines:
+			match = re.search(pattern_doc_id, line)
+			if match:
+				#extract the docid from the line : the first group in the regex (what's between parenthesis)
+				doc_id = int(match.group(1))
+			elif re.search(pattern_doc_end, line) and doc != '' and doc_id != '':
+				#if we reached the end of the document, insert tokens in hashmap and flush variables
+				words = tokenization.TextFile.tokenizeStringSplit(doc)
+				score = 1.0/len(words)
+				for word in words:
+					if not word in ii:
+						ii[word] = {}
+					if not doc_id in ii[word]:
+						ii[word][doc_id] = score
+					else:
+						ii[word][doc_id] += score
+				doc = ''
+				del doc_id
+	return ii
+	
 
--- yac/sample.py
+++ yac/sample.py
@@ -1,4 +1,6 @@
-import time, tokenization
+import time
+import tokenization
+import indexing
 
 # this script just tokenizes a file (relative path from the directory you're in in the command line)
 # if you are at ./YAC and the file is in the folder above, pathToFile = ../la010189 for example
@@ -6,7 +8,12 @@ start = time.clock()
 #t = tokenization.TextFile("pathToFile") # '../../../../Downloads/latimes/la010189'
 #hashmap = t.tokenizeTextFileByDocSplit() #True : without tags (<DOC> for example), False or nothing : tags are included. 
 #print(hashmap)
-testFileList = tokenization.TextFileList.fileListFromPath("folderPath") # '../../../../Downloads/latimes'
-print(len(testFileList.tokenizeTextFilesByDocSplit()))
+"""testFileList = tokenization.TextFileList.fileListFromPath("folderPath") # '../../../../Downloads/latimes'
+print(len(testFileList.tokenizeTextFilesByDocSplit()))"""
+
+ii = indexing.createIndex("../../latimes/la010189")
+print(ii)
+
 end = time.clock()
 print(end - start)
+
